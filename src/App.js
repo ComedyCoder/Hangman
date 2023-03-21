66ab5c8e6a095392003c5edbb8fdca5d6d1b61c2
McClaine Beirne <beirne@gmx.com>
2023-03-21 00:53:25 +0000

    removed un-needed blank spaces

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ function App() {
       <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
             Powered by React
-        
-        
+            
       </header>
       <div className="Game">
       <button onClick={refreshPage}>Click to reload! ↺</button>
